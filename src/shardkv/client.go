338ca7876e1f7033914964600c8c8b872aaf154e
rui.xu <rui.xu@liulishuo.com>
2020-09-22 19:12:59 +0800

    1.补提交 client.go

--- src/shardkv/client.go
+++ src/shardkv/client.go
@@ -8,11 +8,20 @@ package shardkv
 // talks to the group that holds the key's shard.
 //
 
-import "../labrpc"
-import "crypto/rand"
-import "math/big"
-import "../shardmaster"
-import "time"
+import (
+	"crypto/rand"
+	"math/big"
+	"sync"
+	"time"
+
+	"../labrpc"
+	"../shardmaster"
+)
+
+var (
+	clerkID = 1
+	mu      sync.Mutex
+)
 
 //
 // which shard is a key in?
@@ -40,6 +49,11 @@ type Clerk struct {
 	config   shardmaster.Config
 	make_end func(string) *labrpc.ClientEnd
 	// You will have to modify this struct.
+
+	ID           int
+	SerialNumber int64
+	mu           sync.Mutex
+	RaftLeaderID int
 }
 
 //
@@ -56,7 +70,23 @@ func MakeClerk(masters []*labrpc.ClientEnd, make_end func(string) *labrpc.Client
 	ck.sm = shardmaster.MakeClerk(masters)
 	ck.make_end = make_end
 	// You'll have to add code here.
+	ck.RaftLeaderID = 0
+	ck.SerialNumber = 1
+	mu.Lock()
+	ck.ID = clerkID
+	clerkID++
+	mu.Unlock()
+	// You'll have to add code here.
 	return ck
+}
+
+func (ck *Clerk) getUpdateSN() int64 {
+	var ret int64
+	ck.mu.Lock()
+	ret = ck.SerialNumber
+	ck.SerialNumber++
+	ck.mu.Unlock()
+	return ret
 }
 
 //
@@ -65,34 +95,40 @@ func MakeClerk(masters []*labrpc.ClientEnd, make_end func(string) *labrpc.Client
 // keeps trying forever in the face of all other errors.
 // You will have to modify this function.
 //
+
 func (ck *Clerk) Get(key string) string {
-	args := GetArgs{}
-	args.Key = key
+	args := GetArgs{
+		Key:          key,
+		SerialNumber: ck.getUpdateSN(),
+		ClientID:     ck.ID}
+
+	n := ck.RaftLeaderID
 
 	for {
 		shard := key2shard(key)
 		gid := ck.config.Shards[shard]
 		if servers, ok := ck.config.Groups[gid]; ok {
-			// try each server for the shard.
-			for si := 0; si < len(servers); si++ {
-				srv := ck.make_end(servers[si])
-				var reply GetReply
-				ok := srv.Call("ShardKV.Get", &args, &reply)
-				if ok && (reply.Err == OK || reply.Err == ErrNoKey) {
+			var reply GetReply
+			server := ck.make_end(servers[n])
+			if ok := server.Call("ShardKV.Get", &args, &reply); ok {
+				switch reply.Err {
+				case ErrWrongLeader, ErrStaleLeader:
+					n = (n + 1) % len(servers)
+				case ErrWrongGroup:
+					ck.config = ck.sm.Query(-1)
+				case "", ErrNoKey:
+					ck.RaftLeaderID = n
 					return reply.Value
 				}
-				if ok && (reply.Err == ErrWrongGroup) {
-					break
-				}
-				// ... not ok, or ErrWrongLeader
+			} else {
+				n = (n + 1) % len(servers)
 			}
+		} else {
+			ck.config = ck.sm.Query(-1)
 		}
-		time.Sleep(100 * time.Millisecond)
-		// ask master for the latest configuration.
-		ck.config = ck.sm.Query(-1)
+		time.Sleep(OpInterval) // 可能某一轮还在选主，所以最好 sleep 一段时间再去请求
 	}
-
-	return ""
+	// You will have to modify this function.
 }
 
 //
@@ -100,38 +136,47 @@ func (ck *Clerk) Get(key string) string {
 // You will have to modify this function.
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
-	args := PutAppendArgs{}
-	args.Key = key
-	args.Value = value
-	args.Op = op
+	// You will have to modify this function.
+	// args *PutAppendArgs, reply *PutAppendReply
+	args := PutAppendArgs{
+		Key:          key,
+		Value:        value,
+		Op:           op,
+		SerialNumber: ck.getUpdateSN(),
+		ClientID:     ck.ID}
 
+	n := ck.RaftLeaderID
 
 	for {
+
 		shard := key2shard(key)
 		gid := ck.config.Shards[shard]
 		if servers, ok := ck.config.Groups[gid]; ok {
-			for si := 0; si < len(servers); si++ {
-				srv := ck.make_end(servers[si])
-				var reply PutAppendReply
-				ok := srv.Call("ShardKV.PutAppend", &args, &reply)
-				if ok && reply.Err == OK {
+			var reply PutAppendReply
+			server := ck.make_end(servers[n])
+			if ok := server.Call("ShardKV.PutAppend", &args, &reply); ok {
+				switch reply.Err {
+				case ErrWrongLeader, ErrStaleLeader:
+					n = (n + 1) % len(servers)
+				case ErrWrongGroup:
+					ck.config = ck.sm.Query(-1)
+				case "", ErrNoKey:
+					ck.RaftLeaderID = n
 					return
 				}
-				if ok && reply.Err == ErrWrongGroup {
-					break
-				}
-				// ... not ok, or ErrWrongLeader
+			} else {
+				n = (n + 1) % len(servers)
 			}
+		} else {
+			ck.config = ck.sm.Query(-1)
 		}
-		time.Sleep(100 * time.Millisecond)
-		// ask master for the latest configuration.
-		ck.config = ck.sm.Query(-1)
+		time.Sleep(OpInterval)
 	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
-	ck.PutAppend(key, value, "Put")
+	ck.PutAppend(key, value, PutOp)
 }
 func (ck *Clerk) Append(key string, value string) {
-	ck.PutAppend(key, value, "Append")
+	ck.PutAppend(key, value, AppendOp)
 }
